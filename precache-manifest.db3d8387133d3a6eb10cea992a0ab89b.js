0080bdb86f8736dc35a8d82559e211e0ea5c1311
Borislav Atanasov <b.v.atanasov@gmail.com>
2019-06-27 06:30:22 +0100

    Updates

---
+++ precache-manifest.db3d8387133d3a6eb10cea992a0ab89b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24180efeb900a88b3dac84ba106e91a0",
+    "url": "/smart/index.html"
+  },
+  {
+    "revision": "ade4e5e52ef8a25ea81b",
+    "url": "/smart/static/css/main.7cb2c677.chunk.css"
+  },
+  {
+    "revision": "d2c4843a3a54a2018d71",
+    "url": "/smart/static/js/2.138fe95d.chunk.js"
+  },
+  {
+    "revision": "ade4e5e52ef8a25ea81b",
+    "url": "/smart/static/js/main.7ad0eb3c.chunk.js"
+  },
+  {
+    "revision": "b9d752a46d54405debc1",
+    "url": "/smart/static/js/runtime~main.679121d0.js"
+  },
+  {
+    "revision": "9f3339f8088e92a93094dab09aee7d47",
+    "url": "/smart/static/media/17746-682.9f3339f8.png"
+  },
+  {
+    "revision": "302b99d7b753c9d2616aef3f8869e2fc",
+    "url": "/smart/static/media/403-445.302b99d7.png"
+  }
+]);
